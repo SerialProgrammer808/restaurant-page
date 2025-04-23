51d67f091fe56b870f8d392a85b98de0a5ca4f4a
Noah Asano <pili.asano@gmail.com>
2025-04-22 19:28:07 -1000

    Create and link important elements for webpack
    The restaurant page requires that we use webpack to bundle our code for distribution. This commit adds a package.json, index.js, template.html, and a webpack.config.js and links these files together in the proper way to allow for webpacking and devServer

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import pageLoad from "./pageload.js"
+
+let page = document.querySelector("#content");
+pageLoad(page);
---
+++ src/pageload.js
@@ -0,0 +1,4 @@
+export default function pageLoad(page) {
+    page.innerHTML = "<p>This may NOT be working<p>";
+}
+
